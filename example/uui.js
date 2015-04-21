283aafc00b9043c08f02fca1a6a17b7572a69072
Luke Paireepinart <lukepdev@live.com>
2015-04-20 23:01:08 -0500

    added a simple animation to prototype a request animation frame callback loop

--- example/uui.js
+++ example/uui.js
@@ -1,6 +1,8 @@
 
 
 var canvas;
+var x = 0;
+var y = 0;
 
 function InitUUI()
 {
@@ -10,6 +12,8 @@ function InitUUI()
   // resize the canvas to fill browser window dynamically
   window.addEventListener('resize', resizeCanvas, false);
   resizeCanvas();
+
+  drawStuff(); /* start animation loop */
 }
 
 
@@ -21,9 +25,19 @@ function InitUUI()
     * Your drawings need to be inside this function otherwise they will be reset when
     * you resize the browser window and the canvas goes will be cleared.
     */
-    drawStuff();
+    //drawStuff();
   }
 
   function drawStuff() {
-    // do your drawing stuff here
+    x += 2;
+    y += 1;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.arc(x, y, 10, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#003300';
+    context.stroke();
+    window.requestAnimationFrame(drawStuff); /* start event loop. */
   }
